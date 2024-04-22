a246079155f5f36a1a4cf0040c705082e76fb18b
Harsh Raj A <Hraj20000@gmail.com>
2024-04-22 08:47:39 +0530

    navigation included and apps created

--- StudentMentorPortal/login/views.py
+++ StudentMentorPortal/login/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+from django.contrib import messages
+from django.http import JsonResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.template import RequestContext
+from django.views.decorators.cache import cache_control
+from django.views.decorators.csrf import csrf_protect, csrf_exempt, requires_csrf_token
+import requests
+from . import models
 
-# Create your views here.
+
+def landingPage(request):
+    return render(request,"index.html")
+
+def loginPage(request):
+    return render(request,"login.html")
+
+def signUpPage(request):
+    return render(request,"signup.html")
+
+def mentorpage(request):
+    return render(request,"mentor_registration.html")
+
+
+
+
+
+
+

41660895b80756f9478bf61546b9791ab40f7575
Harsh Raj A <Hraj20000@gmail.com>
2024-04-22 13:04:03 +0530

    added few views and models

--- StudentMentorPortal/Student/models.py
+++ StudentMentorPortal/Student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class task(models.Model):
+    work = models.TextField(max_length=220)
+    date = models.DateField
+    completed  = models.BooleanField
+
+class diary(models.Model):
+    date = models.DateField(auto_now_add=True)
+    entry = models.TextField(max_length=500)
--- StudentMentorPortal/login/models.py
+++ StudentMentorPortal/login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    Username = models.CharField(max_length=50,unique=True)
+    password = models.CharField(min_length=6)
+    leader = models.BooleanField(default=False)
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=20,unique=True)
+    experience = models.CharField(max_length=50)

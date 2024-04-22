9b5e3244c63b568380262cd7108de5827da90c2a
Harsh Raj A <Hraj20000@gmail.com>
2024-04-22 10:03:24 +0530

    added models

--- StudentMentorPortal/login/tests.py
+++ StudentMentorPortal/login/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    password = models.CharField(min_length=6)
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=20)
+    experience = models.CharField(max_length=50)
+
+
